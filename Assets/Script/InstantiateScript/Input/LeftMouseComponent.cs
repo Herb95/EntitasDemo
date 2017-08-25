9aea77a6b251f7cc50a914007d7420b06217beca
Herbie <1406912774@qq.com>
2017-08-25 16:45:25 +0800

    添加注释

--- Assets/Script/InstantiateScript/Input/LeftMouseComponent.cs
+++ Assets/Script/InstantiateScript/Input/LeftMouseComponent.cs
@@ -5,31 +5,45 @@ using Entitas;
 using Entitas.CodeGeneration.Attributes;
 
 
-// Unique标签 字面意思唯一 直接在Context访问
+// Unique标签 字面:唯一 可以直接在Context访问
 
+/// <summary>
+/// 鼠标左键
+/// </summary>
 [Input, Unique]
 public class LeftMouseComponent : IComponent
 {
 
 }
+/// <summary>
+/// 鼠标右键
+/// </summary>
 [Input, Unique]
 public class RightMouseComponent : IComponent
 {
 
 }
 
+/// <summary>
+/// 鼠标按下
+/// </summary>
 [Input]
 public class MouseDownComponent : IComponent
 {
     public Vector2 position;
 }
-
+/// <summary>
+/// 鼠标位置
+/// </summary>
 [Input]
 public class MousePositionComponent : IComponent
 {
     public Vector2 position;
 }
 
+/// <summary>
+/// 鼠标抬起
+/// </summary>
 [Input]
 public class MouseUpComponent : IComponent
 {
